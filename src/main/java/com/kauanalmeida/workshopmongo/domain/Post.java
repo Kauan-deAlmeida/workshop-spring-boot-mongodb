331062ae1e217b867a706f5d7757322b9bdd497a
Kauan-deAlmeida <kauan.almeida@sptech.school>
2025-01-17 17:30:42 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/kauanalmeida/workshopmongo/domain/Post.java
@@ -0,0 +1,91 @@
+package com.kauanalmeida.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="post")
+public class Post implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String id;
+	private Date data;
+	private String title;
+	private String bory;
+	private User author;
+	
+	public Post() {}
+
+	public Post(String id, Date date, String title, String bory, User author) {
+		this.id = id;
+		this.data = date;
+		this.title = title;
+		this.bory = bory;
+		this.author = author;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBory() {
+		return bory;
+	}
+
+	public void setBory(String bory) {
+		this.bory = bory;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		return Objects.equals(id, other.id);
+	}
+}
